2d5bfe9ec7f7d78a9d044284eec8edaac6d344ed
tanaseyurina <yurinax529@gmail.com>
2020-07-20 13:24:15 +0900

    Please confir

--- emotions.py
+++ emotions.py
@@ -1,6 +1,9 @@
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
 import cv2
+
+import cv2 as cv
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
@@ -11,13 +14,67 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from datetime import datetime
 
 import os
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from PIL import Image
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import time
+#--------------------------------------------------------------------------
 
-os.makedirs('emotions',exist_ok=True)
+alpha = 0.5
 
+print(''' Simple Linear Blender
+-----------------------
+* Enter alpha [0.0-1.0]: ''')
+input_alpha = float(input().strip())
+if 0 <= alpha <= 1:
+    alpha = input_alpha
+# [load]
+src1 = cv.imread(cv.samples.findFile('3.png'))
+#src2 = cv.imread(cv.samples.findFile('4.jpg'))
+# [load]
+if src1 is None:
+    print("Error loading src1")
+    exit(-1)
+"""
+elif src2 is None:
+    print("Error loading src2")
+    exit(-1)
+"""
+# [blend_images]
+beta = (1.0 - alpha)
+
+
+
+"""
+#------------------------------------------------------------------------------
+# 画像を読み込む。
+img4 = cv2.imread("neko.jpg")
+
+# グレースケールに変換する。
+gray1 = cv2.cvtColor(img4, cv2.COLOR_BGR2GRAY)
+
+# 2値化する。
+thresh, binary = cv2.threshold(gray1, 230, 255, cv2.THRESH_BINARY_INV)
+
+# 輪郭を抽出する。
+contours, hierarchy = cv2.findContours(
+    binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+)
+
+# マスクを作成する。
+mask = np.zeros_like(binary)
+
+# 輪郭内部 (透明化しない画素) を255で塗りつぶす。
+cv2.drawContours(mask, contours, -1, color=255, thickness=-1)
+
+# RGBA に変換する。
+rgba = cv2.cvtColor(img4, cv2.COLOR_RGB2RGBA)
+
+# マスクをアルファチャンネルに設定する。
+rgba[..., 3] = mask
+
+# 保存する。
+cv2.imwrite(r"neko1.png", rgba)
+#-----------------------------------------------------------------------------
+"""
 # Create the model
 model = Sequential()
 
@@ -37,6 +94,7 @@ model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
+
 # emotions will be displayed on your face from the webcam feed
 model.load_weights('date/model.h5')
 
@@ -47,152 +105,145 @@ cv2.ocl.setUseOpenCL(False)
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 # add
-face_cascade_alt = cv2.CascadeClassifier('date/haarcascade_frontalface_alt.xml')
-face_cascade_default = cv2.CascadeClassifier('date/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('date/haarcascade_frontalface_alt.xml')
+facecasc = cv2.CascadeClassifier('date/haarcascade_frontalface_default.xml')
+eyes = cv2.CascadeClassifier('date/haarcascade_righteye_2splits.xml')
 
-# paste_image
-def paste_image(img, rect):
-    (x_tl, y_tl, x_br, y_br) = rect
-    wid = x_br - x_tl
-    hei = y_br - y_tl
+# mosaic
+def mosaic(img, rect, size):
+    (x1, y1, x2, y2) = rect
+    w = x2 - x1
+    h = y2 - y1
+    i_rect = img[y1:y2, x1:x2]
 
-    img_face = cv2.resize(input_img, (wid, hei))
-    paste_img = img.copy()
-    paste_img[y_tl:y_br, x_tl:x_br] = img_face
-    return paste_img
+    i_small = cv2.resize(i_rect, (size, size))
+    i_mos = cv2.resize(i_small, (w, h), interpolation=cv2.INTER_AREA)
 
-# apply_mosaic
-def apply_mosaic(mosaic_img):
-    small_mosaic_image = cv2.resize(mosaic_img, None, fx=0.1, fy=0.1, interpolation=cv2.INTER_NEAREST)
-    return cv2.resize(small_mosaic_image, mosaic_img.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+    img2 = img.copy()
+    img2[y1:y2, x1:x2] = i_mos
+    return img2
+
+# image
+def image(img, rect):
+    
+    (x1, y1, x2, y2) = rect
+    w = x2 - x1
+    h = y2 - y1
+
+    img_face = cv2.resize(img1, (w, h))
+    #img_face = cv2.resize(img, (w, h))
+    img2 = img.copy()
+    img2[y1:y2, x1:x2] = img_face
+    return img2
+
+cou0=cou1=cou2=cou3=cou4=cou5=cou6=cou7=0
 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    recognized_faces = face_cascade_default.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
-    recognized_faces = face_cascade_alt.detectMultiScale(gray, 1.5, 3)
     
-    frame = frame .copy()
-    frame = apply_mosaic(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, 1.5, 3)
+    
 
-    for (x, y, w, h) in recognized_faces:
-
+    for (x, y, w, h) in faces:
+        
         roi_gray = gray[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
         prediction = model.predict(cropped_img)
-        for i in range(0, 1, 1):
-            time.sleep(0.3)
-            maxindex = int(np.argmax(prediction))
-        #maxindex = int(np.argmax(prediction))
+        maxindex = int(np.argmax(prediction))
         
         print(maxindex)
-            
+
+        #img1 = cv2.imread('img/sad/5.png')
+        #ret, img = cap.read()
+        #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #frame=image(img, (x, y, x+w, y+h))
+
         if maxindex == 0:
-                input_img = cv2.imread('img/angry/angry.png')
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
                 ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                img1 = rgba
+                dst = cv.addWeighted(src1, alpha, frame, beta, 0.0)
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
-         
-        elif maxindex == 1:
-                input_img = cv2.imread('img/disgusted/disgusted.png')
-                ret, img = cap.read()
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
-        
-        elif maxindex ==2:
-                input_img = cv2.imread('img/fearful/fearful.png')
-                ret, img = cap.read()
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
-        
+                frame=image(rgba,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
+                #ret, img = cap.read()
+                #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                #frame=mosaic(img, (x, y, x+w, y+h), 10)
+            
         elif maxindex ==3:
-                input_img = cv2.imread('img/happy/happy.png')
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
                 ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                print('ok0')
+                img1 = rgba
+                print('ok')
+                #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                print('ok1')
+                frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))    
+                print('ok2')
+        """    
+        elif maxindex == 1:
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
+                ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                img1 = rgba
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
+                frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
+ 
+        elif maxindex ==2:
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
+                ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                img1 = rgba
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
         
         elif maxindex == 4:
-                input_img = cv2.imread('img/neutral/neutral.png')
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
                 ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                img1 = rgba
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
-        
+                frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
+
         elif maxindex == 5:
-                input_img = cv2.imread('img/sad/sad.jpg')
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
                 ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                img1 = rgba
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
+                frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
+  
+                #img1 = cv2.imread('img/sad/5.png')
+                #ret, img = cap.read()
+                #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                #frame=image(img, (x, y, x+w, y+h))
         
         elif maxindex == 6:
-                input_img = cv2.imread('img/surprised/surprised.png')
+            eye = eyes.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eye:
                 ret, img = cap.read()
+                #img1 = cv2.imread('img/fearful/2.png')
+                img1 = rgba
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                frame=paste_image(img, (x, y, x+w, y+h))
-        
+                frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
+        """
+    
+    
     cv2.imshow('Video', cv2.resize(frame,(600,460),interpolation = cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-#-----------------------------------------------------------------------------------------------------------------------
-#eyes = cv2.CascadeClassifier('date/haarcascade_righteye_2splits.xml')
-
-# eye = eyes.detectMultiScale(roi_gray)
-# import matplotlib.pyplot as plt
-
-# for (ex, ey, ew, eh) in eye:
-    # ret, img = cap.read()
-    # img1 = cv2.imread('img/fearful/2.png')
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # frame=image(img,((ex+x)-20,(ey+y)+30,(ex+ew+x)-20,(ey+eh+y)+30))
-
-# mosaic
-# def mosaic(img, rect, size):
-#     (x1, y1, x2, y2) = rect
-#     w = x2 - x1
-#     h = y2 - y1
-#     i_rect = img[y1:y2, x1:x2]
-
-#     i_small = cv2.resize(i_rect, (size, size))
-#     i_mos = cv2.resize(i_small, (w, h), interpolation=cv2.INTER_AREA)
-
-#     img2 = img.copy()
-#     img2[y1:y2, x1:x2] = i_mos
-#     return img2
-
-#------------------------------------------------------------------------------
-#画像を読み込む。
-# img=Image.open("sunglass.jpg").convert('RGB').save('sunglass_rgba')
-
-# img = cv2.imread("sunglass_rgba.jpg")
-
-# #グレースケールに変換する。
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# #2値化する。
-# thresh, binary = cv2.threshold(gray, 230, 255, cv2.THRESH_BINARY_INV)
-
-# #輪郭を抽出する。
-# contours, hierarchy = cv2.findContours(
-#    binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-# )
-
-# #マスクを作成する。
-# mask = np.zeros_like(binary)
-
-# #輪郭内部 (透明化しない画素) を255で塗りつぶす。
-# cv2.drawContours(mask, contours, -1, color=255, thickness=-1)
-
-# #RGBA に変換する。
-# rgba = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
-
-# #マスクをアルファチャンネルに設定する。
-# rgba[..., 3] = mask
-
-# cv2.imwrite(r"result.png", rgba)
-#-----------------------------------------------------------------------------
